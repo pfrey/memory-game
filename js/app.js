1677aafa33f0ff2d55737654b6bfdd41a92efa98
Patty <patty.fresonke@forumcomm.com>
2018-06-05 11:18:07 -0500

    add classes on click for flip functionality

--- js/app.js
+++ js/app.js
@@ -36,3 +36,10 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(function(card) {
+  card.addEventListener('click', function(c) {
+    card.classList.add('open', 'show');
+  });
+});
